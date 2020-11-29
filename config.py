92c9303090fb324c9bceb592a5fc08af521e1c31
UMUTONIRitha <rithamarie9@gmail.com>
2020-11-29 18:42:54 +0200

    first commit

--- config.py
+++ config.py
@@ -1,20 +1,40 @@
 import os
-
 class Config:
-
+    """
+    General configuration parent class
+    """   
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+         Config: The pareny configuration class with General configuration settings
+    """
     pass
-
-
 class DevConfig(Config):
+    """
+    Dvelopment configuration child class
+    Args:
+         Config: The parent configuration class woth Gneral configuration settings
+    """
     DEBUG = True
-
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
 }
+
+
+
+
+
+
